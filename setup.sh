244068083deed979468b1757d7cbbdbab851b1aa
Bjorn Martens <bjorn.martens@vimec.nl>
2018-09-27 12:50:22 +0200

    Add configuration Xresources for urxvt

--- setup.sh
+++ setup.sh
@@ -12,6 +12,14 @@ DOTFILES="$(dirname $(readlink -f $0))"
 
 # Ubuntu 18.04 locations
 VIMHOME=${HOME}/.vim
+XRESOURCES=${HOME}/.Xresources
 
-ln -s ${DOTFILES}/vim ${VIMHOME}
-ln -s ${DOTFILES}/tmux/tmux.conf ${HOME}/.tmux.conf
+if [ ! -e ${VIMHOME} ]; then
+    ln -s ${DOTFILES}/vim ${VIMHOME}
+fi
+if [ ! -e ${HOME}/.tmux.conf ]; then
+    ln -s ${DOTFILES}/tmux/tmux.conf ${HOME}/.tmux.conf
+fi
+if [ ! -e ${XRESOURCES} ]; then
+    ln -s ${DOTFILES}/Xresources/gruvbox-dark.xresources ${XRESOURCES}
+fi
