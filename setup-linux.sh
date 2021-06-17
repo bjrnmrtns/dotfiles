38ea5411565681ed03e49ba6e9f9e63a1c1c7346
Bjorn Martens <bjorn@expeditious.nl>
2021-06-17 11:15:51 +0200

    update tmux zshrc and made seperate linux setup script

---
+++ setup-linux.sh
@@ -0,0 +1,32 @@
+#!/bin/bash	
+
+set -eu
+
+print_error() {
+    echo "ERROR"
+}
+
+trap print_error ERR
+
+DOTFILES=$(readlink -f .)
+
+# Ubuntu 18.04 locations
+ZSHHOME=${HOME}/.zshrc
+ALIASES=${HOME}/.aliases
+VIMHOME=${HOME}/.vim
+
+
+if [ ! -e ${VIMHOME} ]; then
+    ln -s ${DOTFILES}/vim ${VIMHOME}
+fi
+if [ ! -e ${HOME}/.tmux.conf.linux ]; then
+    ln -s ${DOTFILES}/tmux/tmux.conf.linux ${HOME}/.tmux.conf
+fi
+if [ ! -e ${ZSHHOME} ]; then
+    ln -s ${DOTFILES}/zsh/zshrc ${ZSHHOME}
+fi
+if [ ! -e ${ALIASES} ]; then
+    ln -s ${DOTFILES}/zsh/aliases ${ALIASES}
+fi
+
+chsh -s /bin/zsh
